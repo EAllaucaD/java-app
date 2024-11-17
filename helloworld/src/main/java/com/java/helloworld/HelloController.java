687e87f7cf1c9e21ccdf51ff1427fa6b957b6698
Edwin Allauca <edwindavid21v@gmail.com>
2024-11-16 22:04:59 -0500

    changes

---
+++ helloworld/src/main/java/com/java/helloworld/HelloController.java
@@ -0,0 +1,13 @@
+package com.java.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World with Java - Edwin Allauca";
+    }
+}
--- helloworld/src/main/java/com/java/helloworld/HelloworldApplication.java
+++ helloworld/src/main/java/com/java/helloworld/HelloworldApplication.java
@@ -2,22 +2,10 @@ package com.java.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class HelloworldApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
-	}
-}
-
-@RestController
-class HelloController {
-
-	@GetMapping("/")
-	public String hello() {
-		return "Hello World with Java - Edwin Allauca  ";
 	}
 }
